a12cb1871fcbef45c36fc9b5957cc4959a697e03
Ghozy Alkhairi <92246826+ghozyalkhairi@users.noreply.github.com>
2024-07-29 09:32:40 +0700

    first commit

---
+++ DatabaseHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList
+{
+    class DatabaseHelper
+    {
+        private string _connectionString;
+        public DatabaseHelper(string databasePath)
+        {
+            _connectionString = databasePath;
+        }
+        public void CreateDatabase()
+        {
+            SQLiteConnection.CreateFile("TodoItems.db");
+
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+
+                string createTableQuery = @"CREATE TABLE IF NOT EXISTS ToDoItems (Id INTEGER PRIMARY KEY AUTOINCREMENT,Description TEXT NOT NULL,IsCompleted INTEGER NOT NULL);";
+
+                using (var command = new SQLiteCommand(createTableQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Data.SQLite;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TodoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            DatabaseHelper dbHelper = new DatabaseHelper("Data Source=TodoItems.db;Version=3;");
+            dbHelper.CreateDatabase();
+
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<TodoItem> _todoItems;
+        private string _connectionString = "Data Source=TodoItems.db;Version=3;";
+        public MainWindow()
+        {
+            InitializeComponent();
+            _todoItems = new ObservableCollection<TodoItem>();
+            ToDoListBox.ItemsSource = _todoItems;
+            LoadTodoItems();
+        }
+        private void LoadTodoItems()
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM ToDoItems";
+                using (var command = new SQLiteCommand(query, connection))
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        _todoItems.Add(new TodoItem
+                        {
+                            Id = reader.GetInt32(0),
+                            Description = reader.GetString(1),
+                            IsCompleted = reader.GetInt32(2) == 1,
+                        });
+                    }
+                }
+            }
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string description = NewItemTextBox.Text;
+            if (!string.IsNullOrEmpty(description))
+            {
+                using (var connection = new SQLiteConnection(_connectionString))
+                {
+                    connection.Open();
+                    string query = "INSERT INTO TodoItems (Description, IsCompleted) VALUES (@description, 0)";
+                    using (var command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@description", description);
+                        command.ExecuteNonQuery();
+                    }
+                }
+                _todoItems.Add(new TodoItem { Description = description, IsCompleted = false });
+                NewItemTextBox.Clear();
+            }
+        }
+    }
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
